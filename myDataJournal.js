8688640b058ca1971234a623442b3dc711ad4bd2
Emma Schmitt <emmaschmitt22@gmail.com>
2026-01-17 18:05:30 -0800

    Data Added

--- myDataJournal.js
+++ myDataJournal.js
@@ -1,1 +1,22 @@
 //Hello World!
+
+//Define Array
+
+let weekData = [
+    {day: "Monday", sleepHours: 9, screenTime: 2.5, mood: "fine", caffeineIntake: 1, focusLevel: 3},
+    {day: "Tuesday", sleepHours: 8, screenTime: 1, mood: "tired", caffeineIntake: 2, focusLevel: 5},
+    {day: "Wednesday", sleepHours: 9, screenTime: 3, mood: "fine", caffeineIntake: 1, focusLevel: 6},
+    {day: "Thursday", sleepHours: 10, screenTime: 5.5, mood: "focused", caffeineIntake: 2,focusLevel: 8},
+    {day: "Friday", sleepHours: 9, screenTime: 0, mood: "tired", caffeineIntake: 2,focusLevel: 6},
+    {day: "Saturday", sleepHours: 10, screenTime: 2.5,mood: "energized", caffeineIntake: 1, focusLevel: 7},
+    {day: "Sunday", sleepHours: 9, screenTime: 5, mood: "fine", caffeineIntake: 1, focusLevel: 5},
+];
+
+
+
+/*day (string)
+sleepHours (number)
+screenTime (hours)
+mood (string)
+caffeineIntake (cups)
+focusLevel (1–10)*/
