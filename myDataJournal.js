86440df365bb8268ec3e8a22edc74f085837ec9f
Emma Schmitt <emmaschmitt22@gmail.com>
2026-01-18 18:47:11 -0800

    correlateCaffeineToFocus added and tested

--- myDataJournal.js
+++ myDataJournal.js
@@ -1,5 +1,7 @@
-//Define Array
+//Emma Schmitt
+//Github Repo:
 
+//Define Array
 let weekData = [
     {day: "Monday", sleepHours: 9, screenTime: 2.5, mood: "fine", caffeineIntake: 1, focusLevel: 3},
     {day: "Tuesday", sleepHours: 8, screenTime: 5, mood: "tired", caffeineIntake: 2, focusLevel: 5},
@@ -12,7 +14,7 @@ let weekData = [
 
 
 //Tuesday had the most screen time, and the best focus day was Thursday.
-//I predict that higher caffeine intake helps focus, if capped at 2 cups.
+//I predict that higher caffeine intake helps focus (if capped at 2 cups).
 
 //Function for finding average sleep time
 function averageSleep(log){
@@ -25,8 +27,8 @@ function averageSleep(log){
 
 function numLowCafDays(log){
     let lowCafDays = 0
-    for (let i = 0; i < cafCount.length; i++) {
-        if (cafCount <= 1) {
+    for (let entry of log) {
+        if (entry.caffeineIntake <= 1) {
             lowCafDays++
         }
     }
@@ -34,14 +36,48 @@ function numLowCafDays(log){
 }
 
 
-function mostFrequentMood(log){
-    for (let i = 0; i <= 7; i++) {
-
+function highestScreenTime(log) {
+    let highestScreenTime = log[0].screenTime;
+    for (let entry of log) {
+        if (entry.screenTime > highestScreenTime) {
+            highestScreenTime = entry.screenTime;
+        }
     }
-
+    return highestScreenTime;
 }
 
-//def correlateCaffeineToFocus(log)
+
+
+function correlateCaffeineToFocus(log) {
+    let lowCafHighFocus = 0;
+    let highCafHighFocus = 0;
+    let lowCafLowFocus = 0;
+    let highCafLowFocus = 0;
+
+    for (let entry of log) {
+        if (entry.caffeineIntake <= 1 && entry.focusLevel > 5) {
+            lowCafHighFocus++;
+        } else if (entry.caffeineIntake <= 1 && entry.focusLevel <= 5) {
+            lowCafLowFocus++;
+        } else if (entry.caffeineIntake > 1 && entry.focusLevel > 5) {
+            highCafHighFocus++;
+        } else if (entry.caffeineIntake > 1 && entry.focusLevel <= 5) {
+            highCafLowFocus++;
+        }
+    }
+
+    if (lowCafHighFocus > highCafHighFocus) {
+        return "You have higher focus with lower caffeine intake.";
+    } else if (highCafLowFocus > lowCafHighFocus) {
+        return "You have higher focus with higher caffeine intake.";
+    } else {
+        return "Results inconclusive.";
+    }
+}
+
 
 //TEST CALLS
 console.log(averageSleep(weekData));
+console.log(numLowCafDays(weekData));
+console.log(highestScreenTime(weekData));
+console.log(correlateCaffeineToFocus(weekData));
